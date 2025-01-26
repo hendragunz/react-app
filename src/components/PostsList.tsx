2500fb5a7fbb965e29dc67509bb81eac7e1c05c3
Hendra Gunawan <hendragunz@codecampz.com>
2025-01-26 23:43:08 +0700

    small header tweak

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -22,6 +22,7 @@ function PostList({ isPosting, onStopPosting }) {
           <NewPost
             onSomethingChange={somethingChangeHandler}
             onNameChange={nameChangeHandler}
+            onCancel={onStopPosting}
           />
         </Modal>
       ) : null}
